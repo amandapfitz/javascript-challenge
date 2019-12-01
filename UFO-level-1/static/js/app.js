e29435efac494e6b94265ccf1ff41cd1739a076b
amandapfitz <53943944+amandapfitz@users.noreply.github.com>
2019-11-30 20:42:50 -0500

    Completed filtering table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -38,16 +38,25 @@ button.on("click", function() {
   var filteredData = tableData.filter(sightings => sightings.datetime === inputValue);
 
   console.log(filteredData);
+  
+  
+  // Delete original table items to show only filtered table items
+  // Use the length of the original table to limit the number of rows deleted
+  for (var j = 0; j < tableData.length; j++) {
+  let table = document.querySelector('table');
 
+  // Delete second row so that the header remains
+  table.deleteRow(1);
+  };
 
-
+  // Filter the table based on the user input
   filteredData.forEach(function(ufo) {
     console.log(ufo);
     var row = tbody.append("tr");
     Object.entries(ufo).forEach(function([key, value]) {
       console.log(key, value);
-  //     // Append a cell to the row for each value
-  //     // in the sightings report object
+       // Append a cell to the row for each value
+       // in the sightings report object
       var cell = row.append("td");
       cell.text(value);
     });
