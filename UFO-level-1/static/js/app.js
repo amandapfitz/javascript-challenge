186c1bc960a9a8b3950e452c62d169700b4d51af
amandapfitz <53943944+amandapfitz@users.noreply.github.com>
2019-11-30 20:11:43 -0500

    Table appends at the end instead of filter

---
+++ UFO-level-1/static/js/app.js
@@ -0,0 +1,57 @@
+// from data.js
+// Get a reference to the table body
+var tableData = data;
+var tbody = d3.select("tbody");
+var filterData = data;
+
+// Check that the data using console.log
+console.log(tableData);
+
+// Start adding data to the table
+// sighting values (datetime, city, state, country, shape, duration, comments)
+tableData.forEach(function(sighting) {
+    console.log(sighting);
+    var row = tbody.append("tr");
+    Object.entries(sighting).forEach(function([key, value]) {
+      console.log(key, value);
+  //     // Append a cell to the row for each value
+  //     // in the sighting object
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+
+  // Select the button
+var button = d3.select("#filter-btn");
+
+button.on("click", function() {
+
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+  console.log(tableData);
+
+  var filteredData = tableData.filter(sightings => sightings.datetime === inputValue);
+
+  console.log(filteredData);
+
+
+
+  filteredData.forEach(function(ufo) {
+    console.log(ufo);
+    var row = tbody.append("tr");
+    Object.entries(ufo).forEach(function([key, value]) {
+      console.log(key, value);
+  //     // Append a cell to the row for each value
+  //     // in the sightings report object
+      var cell = row.append("td");
+      cell.text(value);
+    });
+    });
+
+});
+
