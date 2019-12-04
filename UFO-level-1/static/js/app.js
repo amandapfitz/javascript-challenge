d39c8e3b0ed66ef0f57534baea4b8e6ba90f74cd
amandapfitz <53943944+amandapfitz@users.noreply.github.com>
2019-12-04 11:13:53 -0500

    finished product

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,66 +1,61 @@
 // from data.js
-// Get a reference to the table body
 var tableData = data;
+
+// Get a reference to the table body
 var tbody = d3.select("tbody");
-var filterData = data;
+
 
 // Check that the data using console.log
 console.log(tableData);
 
-// Start adding data to the table
+
+// Create function that will add data to the table
 // sighting values (datetime, city, state, country, shape, duration, comments)
-tableData.forEach(function(sighting) {
-    console.log(sighting);
-    var row = tbody.append("tr");
-    Object.entries(sighting).forEach(function([key, value]) {
-      console.log(key, value);
-  //     // Append a cell to the row for each value
-  //     // in the sighting object
-      var cell = row.append("td");
-      cell.text(value);
+function createTable(data) {
+  tbody.html("");
+
+  data.forEach((sighting) => {
+    
+      var row = tbody.append("tr");
+      Object.entries(sighting).forEach(function([key, value]) {
+        console.log(key, value);
+    //     // Append a cell to the row for each value
+    //     // in the sighting object
+        var cell = row.append("td");
+        cell.text(value);
     });
   });
+}
 
-  // Select the button
-var button = d3.select("#filter-btn");
-
-button.on("click", function() {
-
+  // Create function for when the button is selected
+function buttonClick() {
   // Select the input element and get the raw HTML node
   var inputElement = d3.select("#datetime");
 
   // Get the value property of the input element
   var inputValue = inputElement.property("value");
+  let filteredData = tableData;
 
+  // Check the date that is input and the table data
   console.log(inputValue);
   console.log(tableData);
-
-  var filteredData = tableData.filter(sightings => sightings.datetime === inputValue);
-
-  console.log(filteredData);
   
+  // Grab the user input to filter the data
+  if (inputValue) {
+    filteredData = tableData.filter(sightings => sightings.datetime === inputValue);
+}
   
-  // Delete original table items to show only filtered table items
-  // Use the length of the original table to limit the number of rows deleted
-  for (var j = 0; j < tableData.length; j++) {
-  let table = document.querySelector('table');
+// Check to see if the data filtered correctly
+console.log(filteredData);
 
-  // Delete second row so that the header remains
-  table.deleteRow(1);
-  };
+// Build the filtered table
+createTable(filteredData);
+}
 
-  // Filter the table based on the user input
-  filteredData.forEach(function(ufo) {
-    console.log(ufo);
-    var row = tbody.append("tr");
-    Object.entries(ufo).forEach(function([key, value]) {
-      console.log(key, value);
-       // Append a cell to the row for each value
-       // in the sightings report object
-      var cell = row.append("td");
-      cell.text(value);
-    });
-    });
+// Select the button
+var button = d3.select("#filter-btn");
+button.on("click", buttonClick);
 
-});
+// Build the unfiltered table
+createTable(tableData);
 
